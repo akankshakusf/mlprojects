e2cfadcf29a9a5dd6d5382bfc488d9f517472358
akankshakusf <akushwaha2@usf.edu>
2025-02-15 17:17:09 -0500

    Added the code files for prediction pipeline

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def predict_datapoint():
 
 # Conditional statement that runs the application on host 0.0.0.0 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, port=80)
