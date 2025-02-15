0504120b120ca456573d2a0d784ce046f7f3871d
akankshakusf <akushwaha2@usf.edu>
2025-02-15 10:18:44 -0500

    Added Data Tranformation code and pickle file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 
 # Define a dataclass decorator to hold the configuration for data ingestion
 @dataclass
@@ -59,5 +60,8 @@ class DataIngestion:
 
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_tranformation(train_data,test_data)
 
