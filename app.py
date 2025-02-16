ae04dc79dce11c68035659f4d837d855038cc232
akankshakusf <akushwaha2@usf.edu>
2025-02-16 09:00:00 -0500

    added port

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def predict_datapoint():
 
 # Conditional statement that runs the application on host 0.0.0.0 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=8080)
 
